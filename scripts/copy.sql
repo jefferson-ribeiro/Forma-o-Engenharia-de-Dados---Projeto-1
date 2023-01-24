0788d7b4fe59d6f2c07d9c348a940b7c22588a9c
Jefferson Augusto Ribeiro <74573131+jefferson-ribeiro@users.noreply.github.com>
2023-01-24 10:18:07 -0300

    update copy.sql

--- scripts/copy.sql
+++ scripts/copy.sql
@@ -1,72 +1,71 @@
-copy categories 
-from 's3://northwindocopy/categories.csv' 
-CREDENTIALS 'aws_access_key_id=?????;aws_secret_access_key=?????' 
+copy categories
+from 's3://project1-northwind/categories.csv' 
+CREDENTIALS 'aws_access_key_id=xxxxxxxxxxxxxxxxxxxx;aws_secret_access_key=xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx' 
 delimiter ';' 
-region 'sa-east-1'
+region 'us-east-1'
 IGNOREHEADER 1
 DATEFORMAT AS 'YYYY-MM-DD HH:MI:SS'
 removequotes;
 
 copy customers
-from 's3://northwindocopy/customers.csv' 
-CREDENTIALS 'aws_access_key_id=AKIA26DMOKGYWQZ5UO6T;aws_secret_access_key=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' 
+from 's3://project1-northwind/customers.csv' 
+CREDENTIALS 'aws_access_key_id=xxxxxxxxxxxxxxxxxxxx;aws_secret_access_key=xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx' 
 delimiter ';' 
-region 'sa-east-1'
+region 'us-east-1'
 IGNOREHEADER 1
 DATEFORMAT AS 'YYYY-MM-DD HH:MI:SS'
 removequotes;
 
 copy employees 
-from 's3://northwindocopy/employees.csv' 
-CREDENTIALS 'aws_access_key_id=AKIA26DMOKGYWQZ5UO6T;aws_secret_access_key=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' 
+from 's3://project1-northwind/employees.csv' 
+CREDENTIALS 'aws_access_key_id=xxxxxxxxxxxxxxxxxxxx;aws_secret_access_key=xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx' 
 delimiter ';' 
-region 'sa-east-1'
+region 'us-east-1'
 IGNOREHEADER 1
 DATEFORMAT AS 'YYYY-MM-DD HH:MI:SS'
 removequotes;
 
 copy order_details 
-from 's3://northwindocopy/orderdetails.csv' 
-CREDENTIALS 'aws_access_key_id=AKIA26DMOKGYWQZ5UO6T;aws_secret_access_key=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' 
+from 's3://project1-northwind/orderdetails.csv' 
+CREDENTIALS 'aws_access_key_id=xxxxxxxxxxxxxxxxxxxx;aws_secret_access_key=xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx' 
 delimiter ';' 
-region 'sa-east-1'
+region 'us-east-1'
 IGNOREHEADER 1
 DATEFORMAT AS 'YYYY-MM-DD HH:MI:SS'
 removequotes;
 
 copy orders 
-from 's3://northwindocopy/orders.csv' 
-CREDENTIALS 'aws_access_key_id=AKIA26DMOKGYWQZ5UO6T;aws_secret_access_key=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' 
+from 's3://project1-northwind/orders.csv' 
+CREDENTIALS 'aws_access_key_id=xxxxxxxxxxxxxxxxxxxx;aws_secret_access_key=xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx' 
 delimiter ';' 
-region 'sa-east-1'
+region 'us-east-1'
 IGNOREHEADER 1
 DATEFORMAT AS 'YYYY-MM-DD HH:MI:SS'
 removequotes;
 
 copy products 
-from 's3://northwindocopy/products.csv' 
-CREDENTIALS 'aws_access_key_id=AKIA26DMOKGYWQZ5UO6T;aws_secret_access_key=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' 
+from 's3://project1-northwind/products.csv' 
+CREDENTIALS 'aws_access_key_id=xxxxxxxxxxxxxxxxxxxx;aws_secret_access_key=xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx' 
 delimiter ';' 
-region 'sa-east-1'
+region 'us-east-1'
 IGNOREHEADER 1
 DATEFORMAT AS 'YYYY-MM-DD HH:MI:SS'
 removequotes;
 
 copy shippers 
-from 's3://northwindocopy/shippers.csv' 
-CREDENTIALS 'aws_access_key_id=AKIA26DMOKGYWQZ5UO6T;aws_secret_access_key=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' 
+from 's3://project1-northwind/shippers.csv' 
+CREDENTIALS 'aws_access_key_id=xxxxxxxxxxxxxxxxxxxx;aws_secret_access_key=xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx' 
 delimiter ';' 
-region 'sa-east-1'
+region 'us-east-1'
 IGNOREHEADER 1
 DATEFORMAT AS 'YYYY-MM-DD HH:MI:SS'
 removequotes;
 
 copy suppliers 
-from 's3://northwindocopy/suppliers.csv' 
-CREDENTIALS 'aws_access_key_id=AKIA26DMOKGYWQZ5UO6T;aws_secret_access_key=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' 
+from 's3://project1-northwind/suppliers.csv' 
+CREDENTIALS 'aws_access_key_id=xxxxxxxxxxxxxxxxxxxx;aws_secret_access_key=xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx' 
 delimiter ';' 
-region 'sa-east-1'
+region 'us-east-1'
 IGNOREHEADER 1
 DATEFORMAT AS 'YYYY-MM-DD HH:MI:SS'
 removequotes;
-
